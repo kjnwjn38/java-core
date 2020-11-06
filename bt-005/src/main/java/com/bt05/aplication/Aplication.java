abc12e76cd65d10e01d661277ecc7655b1be1c6e
kjnwjn38 <phamnguyenhoang.quan.1412@gmail.com>
2020-11-06 22:33:30 +0700

    bai5

--- bt-005/src/main/java/com/bt05/aplication/Aplication.java
+++ bt-005/src/main/java/com/bt05/aplication/Aplication.java
@@ -1,9 +1,18 @@
 package com.bt05.aplication;
 
+import java.util.Scanner;
+
 public class Aplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("nhap n:");
+		int n = sc.nextInt();
+		double s = 0;
+		for (int i = 1; i <= n; i++) {
+			s += 1.0 / (2 * i);
+		}
+		System.out.println("tong la:" + s);
 
 	}
 
