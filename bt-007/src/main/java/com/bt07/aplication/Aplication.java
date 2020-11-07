cb176e37ae185534561a023411aae58aa3ec06af
kjnwjn38 <phamnguyenhoang.quan.1412@gmail.com>
2020-11-07 08:58:31 +0700

    bai7

---
+++ bt-007/src/main/java/com/bt07/aplication/Aplication.java
@@ -0,0 +1,18 @@
+package com.bt07.aplication;
+
+import java.util.Scanner;
+
+public class Aplication {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("nhap n:");
+		int n = sc.nextInt();
+		double s = 0;
+		for (int i = 1; i <= n; i++) {
+			s += (1.0*i)/(i+1);
+		}
+		System.out.println("tong la:" + s);
+	}
+
+}
